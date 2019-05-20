2b60628e5d51ad066d88fbe90b8b2b1c589f3d50
Jowita <jowita.lapies@gmail.com>
2019-05-20 10:52:14 +0100

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,7 @@
+# require_relative '../lib/concerns/memorable.rb'
+# require_relative '../lib/concerns/findable.rb'
+# require_relative '../lib/concerns/pramable.rb'
+# require_relative '../lib/artist.rb'
+# require_relative '../lib/song.rb'
+# require "pry"
+# binding.pry
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,15 @@
 require 'pry'
 
 class Song
+
+  
   attr_accessor :name
   attr_reader :artist
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@songs = []
 
